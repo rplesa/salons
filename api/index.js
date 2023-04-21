e5d82bacdc3c5c3483569c8b2512b0d6f9a6f557
Rahela Plesa <gheorghitarahela@gmail.com>
2023-04-21 13:46:48 +0300

    changes in port

--- api/index.js
+++ api/index.js
@@ -69,7 +69,7 @@ app.use((err, req, res, next) => {
 
 app.use(compression())
 
-const port=process.env.PORT || 8000
+const port=process.env.PORT || 8080
 app.listen(port, () => {
   connect();
   console.log("Connected with backend");
